9eaf3fdcdb3bd349a693e729981d066cef501b90
jay <jaydhanwalkar123@gmail.com>
2026-05-21 20:41:27 +0530

    Code base updated

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,7 @@ import { FormEvent, useEffect, useMemo, useState } from "react";
 
 const API_BASE = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
 
+
 interface Article {
   _id: string;
   title: string;
